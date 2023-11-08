d2418b0f5126b5d3327d2e6a8d963c7680ff1e1e
Sahithi Yamasani <sahithiyamasani4@gmail.com>
2023-11-08 11:32:32 -0600

    added connection string

--- app.js
+++ app.js
@@ -4,12 +4,41 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var app = express();
+require('dotenv').config();
+const connectionString =process.env.MONGO_CON
+mongoose = require('mongoose');
+mongoose.connect(connectionString);
+
+//Get the default connection
+var db = mongoose.connection;
+//Bind connection to error event
+db.on('error', console.error.bind(console, 'MongoDB connectionerror:'));
+db.once("open", function(){
+console.log("Connection to DB succeeded")});
+
+var Monitor = require("./models/monitor");
+
+async function recreateDB()
+{
+  // Delete everything
+  await Monitor.deleteMany();
+  let instance1 = new Monitor({model:"Monitor 1", screen_size:24,price:199.99});
+  instance1.save().then(doc=>{
+  console.log("First object saved")}
+  ).catch(err=>{
+  console.error(err)
+  });
+}
+let reseed = true;
+if (reseed) {recreateDB();}
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var monitorsRouter = require('./routes/monitor');
 var boardRouter = require('./routes/board');
 var chooseRouter = require('./routes/choose');
-var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));

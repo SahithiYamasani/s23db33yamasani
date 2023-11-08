94aa9fd679efbb34e10d45d8893d6945f9b73382
Sahithi Yamasani <sahithiyamasani4@gmail.com>
2023-11-08 11:22:35 -0600

    added connection string

---
+++ models/monitor.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose")
+const monitorSchema = mongoose.Schema({
+    model: String,
+    screen_size: Number,
+    price: Number
+    })
+    module.exports = mongoose.model("Monitor",
+    monitorSchema)

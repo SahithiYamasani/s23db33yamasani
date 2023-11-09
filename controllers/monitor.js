fededdbed23f86749d7e7d2af003427b36646372
Sahithi Yamasani <sahithiyamasani4@gmail.com>
2023-11-09 13:11:09 -0600

    added put code

--- controllers/monitor.js
+++ controllers/monitor.js
@@ -11,7 +11,7 @@ exports.monitor_list = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
-// for a specific Costume.
+
 // for a specific Costume.
 exports.monitor_detail = async function(req, res) {
     console.log("detail" + req.params.id)
@@ -27,42 +27,67 @@ exports.monitor_detail = async function(req, res) {
 exports.monitor_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: Monitor delete DELETE ' + req.params.id);
 };
+
 // Handle Costume update form on PUT.
-exports.monitor_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: Monitor update PUT' + req.params.id);
-};
+exports.monitor_update_put = async function(req, res) 
+{
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
+    try 
+    {
+        let toUpdate = await monitor.findById(req.params.id)
+        // Do updates of properties
+        if(req.body.model)toUpdate.model = req.body.model;
+        if(req.body.screen_size) toUpdate.screen_size = req.body.screen_size;
+        if(req.body.price) toUpdate.price = req.body.price;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } 
+    catch (err) 
+    {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id}
+        failed`);
+    }
+}
 
     // VIEWS
 // Handle a show all view
-exports.monitor_view_all_Page = async function(req, res) {
-try{
-theMonitors = await monitor.find();
-res.render('monitor', { title: 'Monitor Results', result: theMonitors });
-}
-catch(err){
-res.status(500);
-res.send(`{"error": ${err}}`);
-}
+exports.monitor_view_all_Page = async function(req, res) 
+{
+    try
+    {
+        theMonitors = await monitor.find();
+        res.render('monitor', { title: 'Monitor Results', result: theMonitors });
+    }
+    catch(err)
+    {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 };
 
 // Handle Costume create on POST.
-exports.monitor_create_post = async function(req, res) {
-console.log(req.body)
-let document = new monitor();
+exports.monitor_create_post = async function(req, res) 
+{
+    console.log(req.body)
+    let document = new monitor();
 // We are looking for a body, since POST does not have query parameters.
 // Even though bodies can be in many different formats, we will be picky
 // and require that it be a json object
 // {"costume_type":"goat", "cost":12, "size":"large"}
-document.model = req.body.model;
-document.screen_size = req.body.screen_size;
-document.price = req.body.price;
-try{
-let result = await document.save();
-res.send(result);
-}
-catch(err){
-res.status(500);
-res.send(`{"error": ${err}}`);
-}
+    document.model = req.body.model;
+    document.screen_size = req.body.screen_size;
+    document.price = req.body.price;
+    try
+    {
+        let result = await document.save();
+        res.send(result);
+    }
+    catch(err)
+    {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 };
 
